8fc5ace67187ce38e39a2180dea91045754c9da2
huegort <andrew.beatty@gmit.ie>
2017-11-07 14:10:04 +0000

    animate sprite

--- Week09-badCode/bad array.js
+++ Week09-badCode/bad array.js
@@ -1,7 +1,10 @@
-var images = [];
+var images = {};
 
 for (var i =0; i<10;i++){
-    var obj;
+    images[i]="var"+i;
+    /*var obj;
     obj.id = i;
     obj.name='some String';
+    */
 }
+console.log(images);
